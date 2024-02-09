5bec16a85b708a5d7dd0247edab4b03528b0f78c
gonzamiguel <gonzalo.augustin.miguel@gmail.com>
2024-02-09 17:36:08 -0300

    Navbar al 80% con header y nueva seccion

--- src/utils/BurgerButton.js
+++ src/utils/BurgerButton.js
@@ -1,9 +1,15 @@
 function openMenu() {
     let menu = document.getElementById('menu');
-
     if (menu.classList.contains('hidden')) {
         menu.classList.remove('hidden');
     } else {
         menu.classList.add('hidden');
-    }
+    }
 }
+
+document.getElementById('dropdownButton').addEventListener('click', function() {
+    document.getElementById('dropdownMenu').classList.toggle('hidden');
+});
+
+
+
