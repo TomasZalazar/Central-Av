f7c99322b5d1d883de9732badf407ae808eb81c3
gonzamiguel <gonzalo.augustin.miguel@gmail.com>
2024-02-16 15:34:59 -0300

    Faq y testimonios

--- src/utils/BurgerButton.js
+++ src/utils/BurgerButton.js
@@ -13,3 +13,56 @@ function toggleMenu() {
   // Toggle de la clase 'opacity-100': si la tiene, la quita; si no la tiene, la añade.
   list.classList.toggle('opacity-100');
 }
+
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   const testimonialContainers = document.querySelectorAll('.testimonial');
+//   const nextTestimonialBtn = document.getElementById('next-testimonial-btn');
+
+//   let currentIndex = 0;
+
+//   // Mostrar el primer testimonio
+//   testimonialContainers[currentIndex].classList.remove('hidden');
+
+//   nextTestimonialBtn.addEventListener('click', function() {
+//       // Ocultar el testimonio actual
+//       testimonialContainers[currentIndex].classList.add('hidden');
+      
+//       // Incrementar el índice y mostrar el siguiente testimonio
+//       currentIndex = (currentIndex + 1) % testimonialContainers.length;
+//       testimonialContainers[currentIndex].classList.remove('hidden');
+//   });
+// });
+
+
+
+    document.addEventListener("DOMContentLoaded", function() {
+        const testimonialContainers = document.querySelectorAll('.testimonial');
+        const prevTestimonialBtn = document.getElementById('prev-testimonial-btn');
+        const nextTestimonialBtn = document.getElementById('next-testimonial-btn');
+
+        let currentIndex = 0;
+
+        // Mostrar el primer testimonio
+        testimonialContainers[currentIndex].classList.remove('hidden');
+
+        prevTestimonialBtn.addEventListener('click', function() {
+            // Ocultar el testimonio actual
+            testimonialContainers[currentIndex].classList.add('hidden');
+            
+            // Disminuir el índice y mostrar el testimonio anterior
+            currentIndex = (currentIndex - 1 + testimonialContainers.length) % testimonialContainers.length;
+            testimonialContainers[currentIndex].classList.remove('hidden');
+        });
+
+        nextTestimonialBtn.addEventListener('click', function() {
+            // Ocultar el testimonio actual
+            testimonialContainers[currentIndex].classList.add('hidden');
+            
+            // Incrementar el índice y mostrar el siguiente testimonio
+            currentIndex = (currentIndex + 1) % testimonialContainers.length;
+            testimonialContainers[currentIndex].classList.remove('hidden');
+        });
+    });
+
