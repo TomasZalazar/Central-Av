81140b8be1417d08c16e4b4404c8d4fc14470f1b
TomasZalazar <locoarts123@gmail.com>
2024-02-19 10:55:48 -0300

    cambios page Central Av Apart

--- src/pages/carussel.js
+++ src/pages/carussel.js
@@ -1,84 +1,34 @@
-// Access the Images
-let slideImages = document.querySelectorAll('img');
-// Access the next and prev buttons
-let next = document.querySelector('.next');
-let prev = document.querySelector('.prev');
-// Access the indicators
-let dots = document.querySelectorAll('.dot');
+let currentIndex = 0;
+const images = document.getElementById('imageGallery').getElementsByTagName('img');
+const prevButton = document.querySelector('.gallery button:nth-child(1)');
+const nextButton = document.querySelector('.gallery button:nth-child(2)');
 
-var counter = 0;
+prevButton.addEventListener('click', showPreviousImage);
+nextButton.addEventListener('click', showNextImage);
 
-// Code for next button
-next.addEventListener('click', slideNext);
-function slideNext(){
-slideImages[counter].style.animation = 'next1 0.5s ease-in forwards';
-if(counter >= slideImages.length-1){
-    counter = 0;
-}
-else{
-    counter++;
-}
-slideImages[counter].style.animation = 'next2 0.5s ease-in forwards';
-indicators();
+function showNextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    displayImage(currentIndex);
 }
 
-// Code for prev button
-prev.addEventListener('click', slidePrev);
-function slidePrev(){
-slideImages[counter].style.animation = 'prev1 0.5s ease-in forwards';
-if(counter == 0){
-    counter = slideImages.length-1;
-}
-else{
-    counter--;
-}
-slideImages[counter].style.animation = 'prev2 0.5s ease-in forwards';
-indicators();
+function showPreviousImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    displayImage(currentIndex);
 }
 
-// Auto slideing
-function autoSliding(){
-    deletInterval = setInterval(timer, 3000);
-    function timer(){
-        slideNext();
-        indicators();
+function displayImage(index) {
+    for (let i = 0; i < images.length; i++) {
+        images[i].classList.add('hidden');
     }
+    images[index].classList.remove('hidden');
 }
-autoSliding();
 
-// Stop auto sliding when mouse is over
-const container = document.querySelector('.slide-container');
-container.addEventListener('mouseover', function(){
-    clearInterval(deletInterval);
+document.getElementById('imageGallery').addEventListener('mouseover', function () {
+    prevButton.classList.remove('hidden');
+    nextButton.classList.remove('hidden');
 });
 
-// Resume sliding when mouse is out
-container.addEventListener('mouseout', autoSliding);
-
-// Add and remove active class from the indicators
-function indicators(){
-    for(i = 0; i < dots.length; i++){
-        dots[i].className = dots[i].className.replace(' active', '');
-    }
-    dots[counter].className += ' active';
-}
-
-// Add click event to the indicator
-function switchImage(currentImage){
-    currentImage.classList.add('active');
-    var imageId = currentImage.getAttribute('attr');
-    if(imageId > counter){
-    slideImages[counter].style.animation = 'next1 0.5s ease-in forwards';
-    counter = imageId;
-    slideImages[counter].style.animation = 'next2 0.5s ease-in forwards';
-    }
-    else if(imageId == counter){
-        return;
-    }
-    else{
-    slideImages[counter].style.animation = 'prev1 0.5s ease-in forwards';
-    counter = imageId;
-    slideImages[counter].style.animation = 'prev2 0.5s ease-in forwards';	
-    }
-    indicators();
-}
+document.getElementById('imageGallery').addEventListener('mouseout', function () {
+    prevButton.classList.add('hidden');
+    nextButton.classList.add('hidden');
+});
